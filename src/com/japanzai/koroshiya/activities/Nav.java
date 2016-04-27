922e97cd359d80d61396be165e8170789769076e
koroshiya <korocresent@gmail.com>
2016-04-27 15:19:03 +1000

    Added Marshmallow storage checks

--- src/com/japanzai/koroshiya/activities/Nav.java
+++ src/com/japanzai/koroshiya/activities/Nav.java
@@ -1,15 +1,20 @@
 package com.japanzai.koroshiya.activities;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -23,6 +28,7 @@ import com.japanzai.koroshiya.fragments.RecentFragment;
 import com.japanzai.koroshiya.fragments.SettingFragment;
 import com.japanzai.koroshiya.io_utils.ArchiveParser;
 import com.japanzai.koroshiya.io_utils.ImageParser;
+import com.japanzai.koroshiya.io_utils.StorageHelper;
 import com.japanzai.koroshiya.settings.SettingsManager;
 
 import java.io.File;
@@ -107,7 +113,15 @@ public class Nav extends AppCompatActivity
                     }
                     break;
                 case READ:
-                    frag = new FileChooserFragment(); //TODO: check for read storage permission
+                    if (!StorageHelper.isExternalStorageReadable()){
+                        Snackbar.make(v, "External storage not readable", Snackbar.LENGTH_SHORT).show();
+                        return;
+                    }else if (!hasStoragePermission()){
+                        askStoragePermission();
+                        return;
+                    }else{
+                        frag = new FileChooserFragment();
+                    }
                     break;
                 case CREDITS:
                     frag = new CreditsFragment();
@@ -168,7 +182,7 @@ public class Nav extends AppCompatActivity
         startActivity(browserIntent);
     }
 
-    private void handleFileInput(Uri uri){
+    private void handleFileInput(Uri uri){ //TODO: check for storage permissions
         String filePath = uri.toString();
         File f = new File(filePath);
         boolean supported = ImageParser.isSupportedFile(f);
@@ -195,6 +209,37 @@ public class Nav extends AppCompatActivity
             }else{
                 Toast.makeText(this, R.string.str_unsupported_file_type_or_unreadable_directory, Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    private void openSettingsPage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+
+    public boolean hasStoragePermission(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true;
+        String perm = Manifest.permission.READ_EXTERNAL_STORAGE;
+        return ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void askStoragePermission(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
+        String perm = Manifest.permission.READ_EXTERNAL_STORAGE;
+        ActivityCompat.requestPermissions(this, new String[]{perm}, 0);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean notGranted = grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED;
+        if (notGranted){
+            openSettingsPage("You must give JComic storage permissions");
+        }else{
+            onNavigationDrawerItemSelected(READ);
         }
     }
 
