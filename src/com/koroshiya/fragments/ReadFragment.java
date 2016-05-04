cb8c22a9637fb7b3c4e1a9076e7cd26279c8708f
koroshiya <korocresent@gmail.com>
2016-05-05 09:07:14 +1000

    Added setting for immersive mode

--- src/com/koroshiya/fragments/ReadFragment.java
+++ src/com/koroshiya/fragments/ReadFragment.java
@@ -5,8 +5,6 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -117,6 +115,7 @@ public class ReadFragment extends Fragment {
         SettingsManager.setBacklightAlwaysOn(act, true);
         SettingsManager.setFullScreen(act, true);
         SettingsManager.setActionBarHidden(act, true);
+        SettingsManager.setImmersiveMode(act, true);
     }
 
     @Override
@@ -125,6 +124,7 @@ public class ReadFragment extends Fragment {
         SettingsManager.setBacklightAlwaysOn(act, false);
         SettingsManager.setFullScreen(act, false);
         SettingsManager.setActionBarHidden(act, false);
+        SettingsManager.setImmersiveMode(act, false);
 
         if (cache != null) {
             cache.close();
--- src/com/koroshiya/settings/SettingsManager.java
+++ src/com/koroshiya/settings/SettingsManager.java
@@ -3,10 +3,12 @@ package com.koroshiya.settings;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -219,6 +221,33 @@ public abstract class SettingsManager {
                 actionBar.show();
             }
 
+        }
+    }
+
+    public static void setImmersiveMode(Activity act, boolean setImmersive){
+
+        setImmersive = setImmersive && getPreferences(act).getBoolean(act.getString(R.string.st_immersive), Boolean.parseBoolean(act.getString(R.string.st_immersive_default)));
+
+        View decorView = act.getWindow().getDecorView();
+        if (setImmersive) {
+
+            int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                }
+
+            }
+
+            decorView.setSystemUiVisibility(flags); //TODO: on tap, show nav?
+        }else{
+            decorView.setSystemUiVisibility(0);
         }
     }
 
